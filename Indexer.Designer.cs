b5e14a850c883ffc1e17c95984adfb1f446c8471
Sadie <zulc22db+oss@gmail.com>
2025-05-08 23:13:05 -0700

    initial commit

---
+++ Indexer.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ShoddyLauncher
+{
+    partial class Indexer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.Description = "Sorry for using the shitty tree one. I had no other choice.";
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(332, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "...";
+            // 
+            // Indexer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 68);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Indexer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scanning emulator archives...";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Indexer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ShoddyLauncher
+{
+    public partial class Indexer : Form
+    {
+        List<string> ArchiveFiles;
+        bool Cancelled = false;
+        Task SearchTask;
+
+        public Indexer()
+        {
+            InitializeComponent();
+            progressBar1.Maximum = 1000;
+            ArchiveFiles = new List<string>();
+
+            MessageBox.Show(
+                "Please select a folder to scan for archive files.\n"+
+                "Archives will be extracted when they are needed.",
+            "ShoddyLauncher", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.Cancel) {
+                Cancelled = true;
+                return;
+            }
+
+            string[] files = System.IO.Directory.GetFiles(folderBrowserDialog.SelectedPath);
+            ArchiveFiles.AddRange(files);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (Cancelled) Close();
+            Activate();
+
+            SearchTask = new Task(SearchArchives);
+            SearchTask.Start();
+        }
+
+        private void ProgressTxt(string t) {
+            if (progressBar1.InvokeRequired)
+            {
+                Action a = delegate { ProgressTxt(t); };
+                progressBar1.Invoke(a);
+            }
+            else label1.Text = t;
+        }
+        private void ProgressMax(int max) {
+            if (progressBar1.InvokeRequired) {
+                Action a = delegate { ProgressMax(max); };
+                progressBar1.Invoke(a);
+            } else progressBar1.Maximum = max;
+        }
+        private void ProgressVal(int val) {
+            if (progressBar1.InvokeRequired)
+            {
+                Action a = delegate { ProgressVal(val); };
+                progressBar1.Invoke(a);
+            } else progressBar1.Value = val;
+        }
+
+        private void SearchArchives() {
+            ProgressTxt("Extracting 7z...");
+
+            Directory.SetCurrentDirectory(Path.GetTempPath());
+            string sevenza_path = Directory.GetCurrentDirectory() + @"\7z.exe";
+            File.WriteAllBytes(sevenza_path, ShoddyLauncher.Properties.Resources._7z_exe);
+
+            int count = ArchiveFiles.Count;
+            int index = 0;
+            ProgressMax(count - 1);
+            foreach (string filePath in ArchiveFiles)
+            {
+                ProgressVal(index++);
+                ProgressTxt("Scanning '" + Path.GetFileName(filePath) + "'... (" + (count-index).ToString() + " remain)");
+                Process _7z = new Process();
+                _7z.StartInfo.FileName = sevenza_path;
+                _7z.StartInfo.Arguments = "l -bd \"" + filePath + "\"";
+                _7z.StartInfo.UseShellExecute = false;
+                _7z.StartInfo.RedirectStandardOutput = true;
+                _7z.StartInfo.CreateNoWindow = true;
+                _7z.Start();
+                string list_output = _7z.StandardOutput.ReadToEnd();
+                string[] lines = list_output.Split(new string[]{"\r\n"}, StringSplitOptions.None);
+                if (lines[3].EndsWith("Can not open file as archive"))
+                {
+                    MessageBox.Show("Failed to open: " + filePath);
+                    continue;
+                }
+                int linesUntil = -1;
+                List<string> filesInArchive = new List<string>();
+                foreach (string line in lines)
+                {
+                    if (linesUntil > 0) linesUntil--;
+                    if (line.StartsWith("   Date"))
+                    {
+                        linesUntil = 2;
+                    }
+                    if (linesUntil != 0) continue;
+                    // The file list table has ended
+                    if (line == "-----------------------")
+                    {
+                        break;
+                    }
+                    // File list table has started
+                    string filename = line.Substring(54);
+                    filesInArchive.Add(filename);
+                }
+                continue;
+            }
+            Invoke(new Action(delegate { Close(); }));
+        }
+    }
+}
